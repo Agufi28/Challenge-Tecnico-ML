c21c07d42e25b14559a95d92c3b0d923c442a7fc
agus.mabla <agus.mabla@gmail.com>
2025-03-21 19:18:44 -0300

    Agrego enum para estandarizar los tipos de dato con el objetivo de que los controles sean agnosticos al motor que hay por detrás

---
+++ internal/models/FieldDataTypes.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class FieldDataTypes(Enum):
+    STRING = 1
+    INTEGER = 2
+    DECIMAL = 3
+    BOOLEAN = 4
+    BINARY = 5
+    TIME = 6
+    DATE = 7
+    DATETIME = 8
