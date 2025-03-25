abd7b953445efc7287396fdac034d5d1a1be55fb
agus.mabla <agus.mabla@gmail.com>
2025-03-24 23:19:00 -0300

    Agrego tests para la clase secret. Quedan fuera del directorio app con la intención de que no formen parte del código fuente presente en la imagen de docker

---
+++ tests/test_secret.py
@@ -0,0 +1,26 @@
+from unittest.mock import patch
+import pytest
+from internal.errors.SecretsException import SecretsException # type: ignore
+from internal.secrets.Secrets import Secrets # type: ignore
+
+
+class TestSecrets():
+    @patch("os.getenv", return_value="test_key")
+    def test_getDatabaseEncryptionKey_with_env_var(self, getenv):
+        result = Secrets.getDatabaseEncryptionKey()
+        assert result == "test_key"
+    
+    @patch("os.getenv", return_value=None)
+    def test_getDatabaseEncryptionKey_without_env_var(self, getenv):
+        with pytest.raises(SecretsException):
+            Secrets.getDatabaseEncryptionKey()
+
+    @patch("os.getenv", return_value=None)
+    def test__Secrets__getEnvOrFail_without_env_var(self, getenv):
+        with pytest.raises(SecretsException):
+            Secrets._Secrets__getEnvOrFail("DATABASE_ENCRYPTION_KEY", "databaseEncryptionKey")
+
+    @patch("os.getenv", return_value="test_key")
+    def test_getEnvOrFail_with_env_var(self, getenv):
+        result = Secrets._Secrets__getEnvOrFail("DATABASE_ENCRYPTION_KEY", "databaseEncryptionKey")
+        assert result == "test_key"
