89caa7b2d910886da42efdace5ee91330e7465fe
agus.mabla <agus.mabla@gmail.com>
2025-03-27 02:42:58 -0300

    Etiqueto los endpoints del fron-end para que se muestren correctamente en el SwaggetUI

--- app/frontend/routers/frontend.py
+++ app/frontend/routers/frontend.py
@@ -6,23 +6,23 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory="frontend/templates")
 
-@router.get("/", response_class=HTMLResponse)
+@router.get("/", response_class=HTMLResponse, tags=["GUI"])
 async def root():
     return RedirectResponse(url="/databases")
 
-@router.get("/login", response_class=HTMLResponse)
+@router.get("/login", response_class=HTMLResponse, tags=["GUI"])
 async def read_item(request: Request):
     return templates.TemplateResponse(
         request=request, name="login.html"
     )
 
-@router.get("/databases", response_class=HTMLResponse)
+@router.get("/databases", response_class=HTMLResponse, tags=["GUI"])
 async def read_item(request: Request):
     return templates.TemplateResponse(
         request=request, name="databases.html"
     )
 
-@router.get("/results", response_class=HTMLResponse)
+@router.get("/results", response_class=HTMLResponse, tags=["GUI"])
 async def read_item(request: Request, databaseId: int):
     return templates.TemplateResponse(
         request=request, name="results.html", context={"databaseId": databaseId}
